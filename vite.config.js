921970f9f1ec4e5ef01b80ae3e936b5a606f6c50
Estefany-s <70726379+Estefany-s@users.noreply.github.com>
2024-07-11 19:20:34 -0600

    vite.config added

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// Esto se hizo para que se pudiera desplegar bien en github pages, 
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: '/blackjack-vite/',
+    plugins: []
+});
+
+// Luego habia que hacer el build de nuevo 
